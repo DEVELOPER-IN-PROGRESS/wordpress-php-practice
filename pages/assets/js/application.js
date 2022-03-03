a665792922e008df12a6441e647675a172400e4a
iceweasel <johnsjosephk@pm.me>
2022-03-03 16:14:22 +0530

    header optimizations

--- pages/assets/js/application.js
+++ pages/assets/js/application.js
@@ -64,16 +64,18 @@ function niCloseDropdown(){
 }
 
 function headerMenu(){
-    console.log(window.innerWidth)
-    let length = window.innerWidth ;
+     let length = window.innerWidth ;
     if(length> 767 && length < 992){
        let linklist = document.querySelectorAll('.ni-navmenu__nav-a');
+       let dropDownList = document.querySelectorAll('.ni-header__accordion-panel');
 
-         linklist.forEach( (item) =>{
-         item.addEventListener('click' , (e)=> {e.preventDefault()  } )
-        item.href='#'
-     })
+       linklist.forEach( (item) =>{
+        item.addEventListener('click' , (e)=> {e.preventDefault() ; } )
+       item.href='#'
+    })
+
     }
+
 }
 
 
